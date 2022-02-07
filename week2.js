b5c1141c7bc62a15caa58696e8edfc13bf9f6605
Tyler Capstick <zzznsewzzz@gmail.com>
2022-02-07 16:10:32 -0600

    Readme update

---
+++ week2.js
@@ -0,0 +1,15 @@
+let pizzaPlace = "Bob's Pizza";
+let numberOfToppings = 9;
+
+// console.log(typeof pizzaPlace);
+// console.log(typeof numberOfToppings);
+
+console.log(
+  `"Welcome to ${pizzaPlace}!, home of the ${numberOfToppings} topping pizza!"`
+);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
